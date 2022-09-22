29a00683f544c447f4f240f0c21a12fbc0a0e864
Yurii Repin <spalz@mail.ru>
2022-09-22 16:57:41 +0600

    Dode!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import './index.scss';
+import React, { useState } from "react";
 
-function App() {
+import "./index.scss";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const setPlus = () => {
+    setCount(count + 1);
+  };
+  const setMinus = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button className="minus" onClick={setMinus}>
+          - Минус
+        </button>
+        <button className="plus" onClick={setPlus}>
+          Плюс +
+        </button>
       </div>
     </div>
   );
-}
+};
 
 export default App;
